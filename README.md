07fe2639721bf81c1f07f587e02b30cabfd101ce
The Boss <JanatiCorp.@Alis-MacBook-Air.local>
2016-02-25 18:20:09 -0500

    yelp project

---
+++ README.md
@@ -0,0 +1,24 @@
+<<<<<<< HEAD
+# TipCalculator
+Beginners ios app calculates tip 
+=======
+# Project - *Yelp Clone*  
+
+**Yelp Clone** is a Yelp search app using the [Yelp API](http://www.yelp.com/developers/documentation/v2/search_api).  
+
+Time spent: **2** hours spent in total  
+ 
+##User Stories 
+- [ ] Table rows for each search results should be dynamic height according to the content height.  
+- [ ] Custom cells should have the proper Auto Layout constraints.  
+- [ ] Search bar should be in the navigation bar   
+
+The following **optional** features are implemented  
+- [ ] Search results page  
+- [ ] Implement map view of resturant results  
+- [ ] Implement the resturant detail page.  
+
+The following **additional** features are implemented:  
+  
+- [ ] List anything elese
+>>>>>>> 22594f1ea3b4f53942fcc7db552e6b5df419d709
---
+++ Yelp/BuisnessCell.swift
@@ -0,0 +1,45 @@
+//
+//  BuisnessCell.swift
+//  Yelp
+//
+//  Created by The Boss on 2/25/16.
+//  Copyright © 2016 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class BuisnessCell: UITableViewCell {
+    @IBOutlet weak var thumbView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var ratingsImageView: UIImageView!
+    @IBOutlet weak var reviewsLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var categoriesLabel: UILabel!
+
+    var business: Business! {
+        didSet {
+            nameLabel.text = business.name
+            thumbView.setImageWithURL(business.imageURL!)
+            categoriesLabel.text = business.categories
+            addressLabel.text = business.address
+            reviewsLabel.text = "\(business.reviewCount!) Reviews"
+            ratingsImageView.setImageWithURL(business.ratingImageURL!)
+            distanceLabel.text = business.distance
+            
+        }
+    }
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        thumbView.layer.cornerRadius = 3
+        thumbView.clipsToBounds = true
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
